d2544f821f0a188541ddc4dac076ee8e71bdd154
Mosharof <md5470415@gmail.com>
2025-02-23 22:01:08 +0600

    create a simple default and named export compo

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+// Default export component
+export default function App() {
+
+  const data = {
+    title: 'React Bangla Tutorial',
+    author: 'unknown',
+    content: 'React Componet  Create '
+  }
+  const avatar = "https://i.imgur.com/7vQD0fPs.jpg";
+  const description = "Gregorio Y. Zara";
+
+  const person = {
+    name: "Gregorio Y. Zara",
+    theme: {
+      backgroundColor: "black",
+      color: "pink",
+    },
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <p>I am Learning React js Again today</p>
+      <h3>Title: {data.title}</h3>
+      <h4>Author : {data.author}</h4>
+      <p>Content : {data.content}</p>
+
+      <img
+        className="avatar"
+        src="https://i.imgur.com/7vQD0fPs.jpg"
+        alt="Gregorio Y. Zara"
+      />
+
+      <hr />
+
+      <img className="avatar" src={avatar} alt={description} />;
+
+      <div>
+        <h1>Amazing scientists</h1>
+        <Profile></Profile>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div style={person.theme}>
+        <h1>{person.name}s Todos</h1>
+        <img
+          className="avatar"
+          src="https://i.imgur.com/7vQD0fPs.jpg"
+          alt="Gregorio Y. Zara"
+        />
+        <ul>
+          <li>Improve the videophone</li>
+          <li>Prepare aeronautics lectures</li>
+          <li>Work on the alcohol-fuelled engine</li>
+        </ul>
+      </div>
     </>
   )
 }
 
-export default App
+
+
+
+// Named Export Component
+export function Profile() {
+  return (
+    <div>
+      <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
+    </div>
+  )
+}
